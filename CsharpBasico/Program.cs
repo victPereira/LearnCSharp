183b5834b43aa6697e4a860287060200fbd0c9e0
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-22 23:38:06 +0100

    edit

--- CsharpBasico/Program.cs
+++ CsharpBasico/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Variaveis tipo basico
+/*
+bool
+char
+byte
+long 
+float 
+double
+decimal
+string
+*/
+    
