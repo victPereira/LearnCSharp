3b6d9c4faf850cb21a2d8659a7e8bb8d008eb760
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-24 15:39:59 +0100

    Criando Pasta para estudar dotnet

---
+++ CsharpBasico/GPACalculator/CalculatorGPA.cs
@@ -0,0 +1,54 @@
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        string course1Name = "English 101";
+        string course2Name = "Algebra 101";
+        string course3Name = "Biologyy 101";
+        string course4Name = "Computer Science I";
+        string course5Name = "Psychology 101";
+
+        int course1Credit = 3;
+        int course2Credit = 3;
+        int course3Credit = 4;
+        int course4Credit = 4;
+        int course5Credit = 3;
+
+        int gradeA = 4;
+        int gradeB = 3;
+
+        int course1Grade = gradeA;
+        int course2Grade = gradeB;
+        int course3Grade = gradeB;
+        int course4Grade = gradeB;
+        int course5Grade = gradeA;
+
+        int totalCreditHours = 0;
+        totalCreditHours += course1Credit;
+        totalCreditHours += course2Credit;
+        totalCreditHours += course3Credit;
+        totalCreditHours += course4Credit;
+        totalCreditHours += course5Credit;
+
+        int totalGradePoints = 0;
+        totalGradePoints += course1Credit * course1Grade;
+        totalGradePoints += course2Credit * course2Grade;
+        totalGradePoints += course3Credit * course3Grade;
+        totalGradePoints += course4Credit * course4Grade;
+        totalGradePoints += course5Credit * course5Grade;
+
+        decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+
+        int leadingDigit = (int)gradePointAverage;
+        int trailingDigits = (int)(gradePointAverage * 100) - leadingDigit * 100;
+
+        Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+        Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+        Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+        Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+        Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+        Console.WriteLine($"Final GPA: {leadingDigit}.{trailingDigits}");
+    }
+}
