0fd6db7717114c9cde4e793c0d70b2cf49848787
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-24 22:34:22 +0100

    edit

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First Roll: {roll1}");
+Console.WriteLine($"Second Roll: {roll2}");
+Console.WriteLine($"Third Roll: {roll3}");
+
+Console.Write("-----------------------------");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
+
+
+
+
+
+
