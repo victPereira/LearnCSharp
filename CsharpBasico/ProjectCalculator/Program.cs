75ac35854338835ef96ab6b07e7f7468f648088f
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-24 14:29:30 +0100

    edit

--- CsharpBasico/ProjectCalculator/Program.cs
+++ CsharpBasico/ProjectCalculator/Program.cs
@@ -1,5 +1,4 @@
-int divisao = 5;
-
+int currentAssignments = 5;
 
 int sophia1 = 93;
 int sophia2 = 87;
@@ -30,13 +29,13 @@ int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
 int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
 int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
 
-decimal sophiaScore = (decimal) sophiaSum / divisao;
-decimal nicolasScore = (decimal) nicolasSum / divisao;
-decimal zahirahScore = (decimal) zahirahSum / divisao;
-decimal jeongScore = (decimal) jeongSum / divisao;
+decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
+decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
+decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
+decimal jeongScore = (decimal) jeongSum / currentAssignments;
 
-Console.WriteLine("Sophia: " + sophiaScore + " A");
-Console.WriteLine("Nicolas: " + nicolasScore + " B");
-Console.WriteLine("Zahirah: " + zahirahScore + " B");
-Console.WriteLine("Jeong: " + jeongScore + " A");
-
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
+Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
+Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
