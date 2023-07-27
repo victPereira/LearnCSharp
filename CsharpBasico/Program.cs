fa4d8dadeae7b59257b8ac3e6bde2a263a4110d0
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-27 16:59:45 +0100

    Ultima ediçao de estudos do dia 27-07

--- CsharpBasico/Program.cs
+++ CsharpBasico/Program.cs
@@ -301,3 +301,45 @@ Console.Write("------Metodo que retornam um valor booliano---\n");
 string pangram = "Uma frase nada com nada.";
 Console.WriteLine(pangram.Contains("frase"));
 Console.WriteLine(pangram.Contains("valor"));
+
+Console.Write("------------------------USO DE OPERADOR CONDICIONAL -------------------\n");
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+Console.Write("------------------------DESAFIO CARA OU COROA -------------------\n\n");
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "hrads" : "tails");
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
