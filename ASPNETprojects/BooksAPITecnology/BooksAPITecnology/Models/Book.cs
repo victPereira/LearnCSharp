b1337bf1a27f3ebb5250c708698416c30760fd5b
Victor Pereira <vic.torpereira@hotmail.com>
2023-07-30 17:29:31 +0100

    Criando GoogleBooksSearch

---
+++ ASPNETprojects/BooksAPITecnology/BooksAPITecnology/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksAPITecnology.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+
+}
---
+++ ASPNETprojects/BooksAPITecnology/BooksAPITecnology/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksAPITecnology.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
